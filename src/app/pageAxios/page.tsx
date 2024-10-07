0db8626d077c8daa78cc360f4511a33f2ee938a8
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-10-07 10:26:06 -0300

    useeffect

--- src/app/pageAxios/page.tsx
+++ src/app/pageAxios/page.tsx
@@ -1,70 +1,63 @@
 "use client"
-import {useState, useEffect, Suspense} from "react"
-import Image from "next/image"
-import {api} from "@/constants/api"
-import { truncateSync } from "fs";
+import { useState, useEffect } from "react"
+import { api } from "@/constants/api"
 
-interface IData 
-{
+interface IData {
     name: string;
     image: string;
-
-    //td string para minimizar os bugs
 }
 
-const pageAxios = () =>
-{
+const PageAxios = () => {
     const [data, setData] = useState<IData[]>([])
     const [erro, setErro] = useState<boolean>(false)
     const [msg, setMsg] = useState<string>("Não foi possível buscar dados")
     const [page, setPage] = useState<string>("")
 
-
     useEffect(() => {
+        if (page) { // Verifica se o valor de page não está vazio
+            api.get(`/characters?page=${page}&limit=10`)
+                .then((res) => {
+                    setErro(false)
+                    setData(res.data.items)
+                })
+                .catch((error) => {
+                    console.log(error);
+                    if (error.response && error.response.status === 404) {
+                        setMsg("Página não encontrada");
+                    }
+                    setErro(true)
+                });
+        }
+    }, [page]) // A requisição é feita sempre que o valor de page mudar
 
-    return () => {
-        api.get(`/characters?page=${page}&limit=10`).then((res)=>{
-            setErro(false)
-            console.log(res.data.items)
-            setData(res.data.items)
-        }).catch((error)=> 
-        {
-            console.log(error);
-            if(error.response.status === 404)
-            {
-                setMsg("Página não encontrada");
-            }
-         
-            setErro(true)
-        })
-    }
-    }, [page])
-
-    // dxcytsdvfuye4rfirbe uhgnthot6hg8u4oimjtgpi
-
-
-    return(
+    return (
         <>
-        <div className="h-screen overflow-y-auto object-cover overflow-hidden ">
-        <h1>Pagina com use effect e axios</h1>
-
-        {erro && <h5>{msg}</h5> }
-        <input type="text" value={page} onChange={(e)=> setPage(e.target.value)} placeholder="1/6"/>
-
-        <div className="flex flex-wrap gap-3 justify-center items-center">
-            {data.map((item, index) => {
-                return(
-                <div className="rounded-lg hover:" key={index}>
-                    <h2>{item.name}</h2>
-                    <img className="h-60 w-auto object-cover rounded-lg" src={item.image} alt="" width={300} height={300}></img>
+            <div className="h-screen overflow-y-auto object-cover overflow-hidden">
+                <h1>Página com useEffect e Axios</h1>
+                {erro && <h5>{msg}</h5>}
+                <input 
+                    type="text" 
+                    value={page} 
+                    onChange={(e) => setPage(e.target.value)} 
+                    placeholder="1/6" 
+                />
+                <div className="flex flex-wrap gap-3 justify-center items-center">
+                    {data.map((item, index) => (
+                        <div className="rounded-lg hover:" key={index}>
+                            <h2>{item.name}</h2>
+                            <img 
+                                className="h-60 w-auto object-cover rounded-lg" 
+                                src={item.image} 
+                                alt="" 
+                                width={300} 
+                                height={300} 
+                            />
+                        </div>
+                    ))}
                 </div>
-                )
-            })}
-        </div>
-
-        </div>
+            </div>
         </>
     )
 }
 
-export default pageAxios;
+export default PageAxios;
